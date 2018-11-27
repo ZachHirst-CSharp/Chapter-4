fe0c20321c119112a7dd0da186e44282748708ae
Zach <zach_hirst@live.com>
2018-11-27 13:18:39 -0600

    Push

---
+++ CheckCredit/CheckCredit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CheckCredit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double purchaseprice;
+
+            WriteLine("Please enter the amount you spent on that item you purchased. ");
+            purchaseprice = Convert.ToDouble(ReadLine());
+
+            if(purchaseprice > 5000)
+            {
+                WriteLine("ERROR 404");
+                WriteLine("This ammount exceeds your credit limit ");
+            }
+            else
+            {
+                WriteLine("I hope you enjoy your item");
+                ReadLine();
+            }
+        }
+    }
+}
