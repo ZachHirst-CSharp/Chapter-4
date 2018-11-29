2286361ff33fc061876b10de45b7c47d807221ac
Zach Hirst <zach_hirst@live.com>
2018-11-29 09:43:49 -0600

    Push

---
+++ NumberPicker/NumberPicker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine(" Please enter a number between 1-3. ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if(number <= 1 && number > 2 && number > 2 && number <= 3)
+            {
+                Console.WriteLine(" Ha Loser ");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("Congrats, you win! ");
+            }
+            else  
+            {
+                Console.WriteLine(" ERROR 404 ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
