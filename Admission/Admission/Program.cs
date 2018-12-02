8efce1053607747b85a4928a2c8a94e0051daffe
Zach Hirst <zach_hirst@live.com>
2018-12-02 10:52:30 -0600

    Push

---
+++ Admission/Admission/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Admission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double highschoolGPA;
+            double admissiontestscore;
+
+            WriteLine("Please enter your High School GPA.");
+            highschoolGPA = Convert.ToDouble(ReadLine());
+            WriteLine("Now enter your Admissions Test Score for calculation. ");
+            admissiontestscore = Convert.ToDouble(ReadLine());
+
+            if(highschoolGPA >= 3.0)
+            {
+                if(admissiontestscore >= 60)
+                {
+                    WriteLine("Congratulations you're admitted!");
+                }
+                else
+                {
+                    WriteLine("I'm sorry your application has been rejected. ");
+                }
+            }
+            else if(highschoolGPA < 3.0)
+            {
+                if(admissiontestscore >= 80)
+                {
+                    WriteLine("Congratulations you're admitted!");
+                }
+                else
+                {
+                    WriteLine("I'm sorry your application has been rejected. ");
+                }
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ Hurricane/Hurricane/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Hurricane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int windspeed;
+
+            WriteLine("Please enter the wind speed of supposed hurricane");
+            windspeed = Convert.ToInt32(ReadLine());
+
+            if(windspeed < 74)
+            {
+                WriteLine("The storm you are experincing is not a hurricane. ");
+            }
+            else if(windspeed >= 74 && windspeed < 96)
+            {
+                WriteLine("This is a catagory 1 hurricane. ");
+            }
+            else if(windspeed >= 96 && windspeed < 111)
+            {
+                WriteLine("This is a catagory 2 hurricane. ");
+            }
+            else if(windspeed >= 111 && windspeed < 130)
+            {
+                WriteLine("This is a catagory 3 hurricane. ");
+            }
+            else if(windspeed >= 130 && windspeed < 157)
+            {
+                WriteLine("This is a catagory 4 hurricane. ");
+            }
+            else if(windspeed >= 157)
+            {
+                WriteLine("This is a catagory 5 hurricane. ");
+            }
+            ReadKey();13
+        }
+    }
+}
